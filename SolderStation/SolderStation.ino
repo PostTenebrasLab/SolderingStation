1676db24e6ad5cf65ecd16135cbb9b991420cdbc
Sebastien Chassot (sinux) <seba.ptl@sinux.net>
2015-08-13 19:32:52 +0200

    make a first draft for pwm support and add PTL logo

--- SolderStation/SolderStation.ino
+++ SolderStation/SolderStation.ino
@@ -10,54 +10,20 @@
 #include <Adafruit_QDTech.h> // Hardware-specific library
 #include <SPI.h>
 
+#include "SolderStation.h"
 #include "iron.h"
 #include "stationLOGO.h"
 
 
-#define VERSION "1.5"		//Version der Steuerung
-#define INTRO
-
-#define sclk 	13		// Don't change
-#define mosi 	11		// Don't change
-#define cs_tft	10		// 
-
-
-//V1.5
-#define dc   	9		// 8
-#define rst  	12		// 9 
-
-/*
-//V1.4
-#define dc   	8
-#define rst  	9
-*/
-
-#define STANDBYin A4
-#define POTI   	A5
-#define TEMPin 	A7
-#define PWMpin 	3
-#define BLpin		5
-
-#define CNTRL_GAIN 10
-
-#define DELAY_MAIN_LOOP 	10
-#define DELAY_MEASURE 		50
-//#define ADC_TO_TEMP_GAIN 	0.415
-#define ADC_TO_TEMP_GAIN 	0.53 //Mit original Weller Station verglichen
-#define ADC_TO_TEMP_OFFSET 25.0
-#define STANDBY_TEMP			175
-
-#define OVER_SHOT 			2
-#define MAX_PWM_LOW			180
-#define MAX_PWM_HI			210		//254
-#define MAX_POTI				400		//400Grad C
-
-#define PWM_DIV 1024						//default: 64   31250/64 = 2ms
-
 Adafruit_QDTech tft = Adafruit_QDTech(cs_tft, dc, rst);  // Invoke custom library
 
-int pwm = 0; //pwm Out Val 0.. 255
-int soll_temp = 300;
+float pwm = 0; //pwm Out Val 0.. 255
+int target = 25;
+int actual_temp = 25;
+int soll_temp_tmp;
+unsigned long last_time;
+long sum = 0;
+long dt = 0;
 boolean standby_act = false;
 
 void setup(void) {
@@ -84,13 +50,14 @@ void setup(void) {
 	
 	//Print station Logo
 	tft.drawBitmap(2,1,stationLOGO1,124,47,QDTech_GREY);
-	
-	tft.drawBitmap(3,3,stationLOGO1,124,47,QDTech_YELLOW);		
-	tft.drawBitmap(3,3,stationLOGO2,124,47,Color565(254,147,52));	
+
+	tft.drawBitmap(3,3,stationLOGO1,124,47,QDTech_YELLOW);
+	tft.drawBitmap(3,3,stationLOGO2,124,47,Color565(254,147,52));
 	tft.drawBitmap(3,3,stationLOGO3,124,47,Color565(255,78,0));
 	
 	//BAcklight on
 	digitalWrite(BLpin, HIGH);
+	last_time = micros();
 	
 	
 #if defined(INTRO)
@@ -158,63 +125,57 @@ void setup(void) {
 }
 
 void loop() {
+
+	int old_temp = actual_temp;
+	getTemperature();
+	// TODO don't
+	target = map(analogRead(POTI), 0, 1023, 0, MAX_POTI);
 	
-	int actual_temperature = getTemperature();
-	soll_temp = map(analogRead(POTI), 0, 1024, 0, MAX_POTI);
-	
-	
-	//TODO: Put in Funktion
-	tft.setCursor(2,55);
-	if (digitalRead(STANDBYin) == true)
-		tft.setTextColor(QDTech_BLACK);
+	if (!digitalRead(STANDBYin))
+		isStandby();
 	else
 		tft.setTextColor(QDTech_WHITE);
-	tft.print("SB");
-	
-	//
-	
-	int soll_temp_tmp = soll_temp;
-	
-	if (digitalRead(STANDBYin) == false)
-		standby_act = true;
-	else
-		standby_act = false;
-	
-	if (standby_act && (soll_temp >= STANDBY_TEMP ))
-		soll_temp_tmp = STANDBY_TEMP;
-	
-	
-	
-	int diff = (soll_temp_tmp + OVER_SHOT)- actual_temperature;
+
+
+	/* PWM */
+	dt = micros() - last_time;
+	/* proportional */
+	pwm = Kp*actual_temp-old_temp;
+	/* integral */
+	sum += (actual_temp-target)*dt;
+	pwm += Ki*sum;
+	/* derivativ */
+	pwm += Kd*(actual_temp-old_temp)/dt;
+
+	// TODO remove this when PWM work
+	int diff = (soll_temp_tmp + OVER_SHOT)- actual_temp;
 	pwm = diff*CNTRL_GAIN;
 	
 	int MAX_PWM;
 
 	//Set max heating Power 
-	MAX_PWM = actual_temperature <= STANDBY_TEMP ? MAX_PWM_LOW : MAX_PWM_HI;
+	MAX_PWM = actual_temp <= STANDBY_TEMP ? MAX_PWM_LOW : MAX_PWM_HI;
 	
 	//8 Bit Range
 	pwm = pwm > MAX_PWM ? pwm = MAX_PWM : pwm < 0 ? pwm = 0 : pwm;
 	
-	
-	
 	//NOTfall sicherheit / Spitze nicht eingesteckt
-	if (actual_temperature > 550){
+	if (actual_temp > MAX_POTI + 50){
 		pwm = 0;
-		actual_temperature = 0;
+		actual_temp = 0;
 	}
 	
 	
 	analogWrite(PWMpin, pwm);
-	//digitalWrite(PWMpin, LOW);
-	
-	writeHEATING(soll_temp, actual_temperature, pwm);
+
+	// TODO remove actual_temperature from writeHEATING() cos is a global variable
+	writeHEATING(target, actual_temp, pwm);
 	
 	delay(DELAY_MAIN_LOOP);		//wait for some time
 }
 
 
-int getTemperature()
+void getTemperature()
 {
   analogWrite(PWMpin, 0);		//switch off heater
   delay(DELAY_MEASURE);			//wait for some time (to get low pass filter in steady state)
@@ -222,7 +183,7 @@ int getTemperature()
   Serial.print("ADC Value ");
   Serial.print(adcValue);
   analogWrite(PWMpin, pwm);	//switch heater back to last value
-  return round(((float) adcValue)*ADC_TO_TEMP_GAIN+ADC_TO_TEMP_OFFSET); //apply linear conversion to actual temperature
+  actual_temp = round(((float) adcValue)*ADC_TO_TEMP_GAIN+ADC_TO_TEMP_OFFSET); //apply linear conversion to actual temperature
 }
 
 
@@ -345,7 +306,19 @@ void writeHEATING(int tempSOLL, int tempVAL, int pwmVAL){
 	
 }
 
+/*
+ * turn the screen off and change target temp to STANDBY_TEMP
+ */
+void isStandby(){
 
+	tft.setCursor(2,55);
+	tft.setTextColor(QDTech_BLACK);
+	tft.print("Standby");
+	target = STANDBY_TEMP;
+
+	(standby_act && (target >= STANDBY_TEMP ))? soll_temp_tmp = STANDBY_TEMP: soll_temp_tmp = target;
+	standby_act = !digitalRead(STANDBYin);
+}
 
 
 uint16_t Color565(uint8_t r, uint8_t g, uint8_t b) {
